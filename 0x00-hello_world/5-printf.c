bc6801a48575a7c86514ba1eaece2718c861760e
Yehenew-Abera <yehenew.abera@gmail.com>
2022-12-09 13:33:37 -0800

    Hello, puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * Use the function printf
- * Your program should return 0
+ * main - initial
+ * Return: return 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
