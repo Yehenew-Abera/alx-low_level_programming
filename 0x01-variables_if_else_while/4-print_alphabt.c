1c948dcbc0ddc5d295abb989e3071b5e46f9cf1d
Yehenew-Abera <yehenew.abera@gmail.com>
2022-12-12 14:55:59 -0800

    Numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,16 @@
  * Return: Always return 0
  */
 int main(void)
-{char ch;
+{
+	char ch;
 	
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
+
 		if (ch != 'e' && ch != 'q')
 			putchar(ch);
 	}
-
 	putchar('\n');
-	
 	return (0);
 }
 
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - returns from 0-9
+ *
+ * Return: returns 0
+ */
+int main(void)
+{	int num;
+	
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
