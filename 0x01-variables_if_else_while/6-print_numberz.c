05063b5dfc8e0eab9b6c4cc90778e22846d166dc
Yehenew-Abera <yehenew.abera@gmail.com>
2022-12-13 10:49:40 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+	
+	putchar('\n');
+
+	return (0);
+
+}
