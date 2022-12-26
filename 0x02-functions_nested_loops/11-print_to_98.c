a06f0aee844385f4604e35ea49d5b431aa960ab0
Yehenew-Abera <yehenew.abera@gmail.com>
2022-12-26 12:19:26 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print upto 98
+ * @n: starting number
+ * Return: 0 or 1
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%i, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+	printf("98");
+	_putchar('\n');
+}
