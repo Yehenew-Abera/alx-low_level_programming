32a0fd705060969c3a86deff1943abc58b127c78
Yehenew-Abera <yehenew.abera@gmail.com>
2022-12-26 11:55:08 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 	while (i <= 9)
 	{
 		j = 0;
-		while (j <= 0)
+		while (j <= 9)
 		{
 			k = i * j;
 			if (j == 0)
@@ -23,7 +23,7 @@ void times_table(void)
 			}
 			else if (k < 10)
 			{
-				_puychar(' ');
+				_putchar(' ');
 				_putchar(k + '0');
 			}
 			else
