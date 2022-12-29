300bd733bbeb54a6840fee95557beed501b7b36f
Yehenew-Abera <yehenew.abera@gmail.com>
2022-12-29 03:37:07 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - peints all combination of single digits
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	}
+}
+
