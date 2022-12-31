e04b6d928795a6e39865df7760425458abfb4ae0
Yehenew-Abera <yehenew.abera@gmail.com>
2022-12-31 08:39:16 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isuppe - checks if c is upper or not
+ * _isupper - checks if c is upper or not
  * Return: returns 1 for upper c or 0 for lower c
  * @c: value for checking
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 98)
 		return (1);
 	else
 		return (0);
