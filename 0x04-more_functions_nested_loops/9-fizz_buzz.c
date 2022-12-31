f944c57af1a6e0bb8283415a306fe3989addcd74
Yehenew-Abera <yehenew.abera@gmail.com>
2022-12-31 09:38:59 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,15 +4,14 @@
  * main - entry point
  * Return: Always 0
  */
-int main()
-
+int main(void)
 {
-	int i, n = 100 ;
-	
+	int i, n = 100;
+
 	for (i = 1; i <= n; i++)
 	{
 		if (i % 15 == 0)
-			printf ("FizzBuzz ");
+			printf("FizzBuzz ");
 		else if ((i % 3) == 0)
 			printf("Fizz ");
 		else if ((i % 5) == 0)
@@ -21,5 +20,5 @@ int main()
 			printf("%d ", i);
 	}
 	printf("\n");
-	return 0;
+	return (0);
 }
