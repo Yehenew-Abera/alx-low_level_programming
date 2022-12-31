c47a73d6c7d6a9af93f09fbc6a23b51e6a695d8f
Yehenew-Abera <yehenew.abera@gmail.com>
2022-12-31 08:58:42 -0800

    Print_most_number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * main - prints 0-9 except 2 and 4
- * @c: numbers to be add
+ * print_most_numbers - prints 0-9 except 2 and 4
  * Return: 0 or 1
  */
 void print_most_numbers(void)
